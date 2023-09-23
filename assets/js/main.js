b6fbbe3c783103bd025f616a0208bbea531c5847
faisal <faisaldevbhaluka@gmail.com>
2023-09-23 22:29:21 +0600

    now create home page and style header section and banner section

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+"use strict";
+
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".main-menu ul");
+
+hamburger.addEventListener("click", () => {
+   hamburger.classList.toggle("active");
+   navMenu.classList.toggle("active");
+})
+
+
+
+
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
